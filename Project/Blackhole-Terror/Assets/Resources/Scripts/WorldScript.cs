fcd490ed876433c0d94b68bbfb946236a4384e27
fvandepitte <frederiek.vandepitte@gmail.com>
2015-01-24 00:37:53 +0100

    Update ignore

---
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/WorldScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldScript : MonoBehaviour {
+    public float speed = 10f;
+    public float Delay = 5f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Delay > 0)
+        {
+            Delay -= Time.deltaTime;
+        }
+        else
+        {
+            Physics2D.gravity = Vector2.Lerp(Physics2D.gravity, new Vector2(0f, 9.81f), speed);
+        }
+	}
+}
