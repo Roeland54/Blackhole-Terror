70af97e3f961b3491468ec60a2fa5c81ecceec61
Roeland Lutters <Roeland.lutters@telenet.be>
2015-01-24 00:59:37 +0100

    updated Player movement controller

---
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/PlayerMovementController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	
+	public float maxSpeed = 10f;
+	
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround; 
+	public float jumpForce = 10f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius);
+
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		
+	}
+
+	void Update ()
+	{
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+
+	}
+
+
+}
