e1bcb59fb0d50ac7184de8e464b917ca1df61fe0
Roeland Lutters <Roeland.lutters@telenet.be>
2015-01-23 23:30:01 +0100

    Update CharacterController

---
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour 
+{
+
+	public float maxSpeed = 10f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+	}
+	
+
+}
