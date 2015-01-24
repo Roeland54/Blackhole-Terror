26bbfd11d024842d06779d2d7170a8b51137c53e
Disati <info@disati.be>
2015-01-24 12:14:08 +0100

    Player die

--- Project/Blackhole-Terror/Assets/Resources/Scripts/Transform/SizeBouncerXY.cs
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/Transform/SizeBouncerXY.cs
@@ -1,19 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SizeBouncer : MonoBehaviour {
+public class SizeBouncerXY : MonoBehaviour {
 
 	public float duration = 1.0F;
 	public float minIntensity = 1.0f;
 	public float maxIntensity = 2.0f;
-	public float sizeX;
 	public float sizeZ;
 	
 	void Update() {
 
 		float phi = Time.time / duration * 2 * Mathf.PI;
 		float amplitude = Mathf.Cos(phi) * minIntensity + maxIntensity;
-		gameObject.transform.localScale = new Vector3(sizeX, amplitude, sizeZ);
+		gameObject.transform.localScale = new Vector3(amplitude, amplitude, sizeZ);
 		//light.intensity = amplitude;
 	}
 }
