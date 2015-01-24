c9643933b6b522471e509c6687664764834519e6
Disati <info@disati.be>
2015-01-24 16:16:21 +0100

    Teleporter

---
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/Transform/PlayerTeleporter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTeleporter : MonoBehaviour {
+
+	public float teleportDelay = 0.1F;
+	private float passiveClock;
+	public Transform directionA;
+	//public Transform directionB;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		passiveClock = passiveClock + Time.deltaTime;
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		print ("Collission detected!");
+		if (other.gameObject.tag == "Player") {
+			print("Player teleported!");
+			if (passiveClock > teleportDelay) {
+				other.transform.position = directionA.position;
+					passiveClock = 0.0f;
+				}
+			}
+		}
+
+}
