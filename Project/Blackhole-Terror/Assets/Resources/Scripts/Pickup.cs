7af3f328b0349b0e6f95ed24a255bcd98c1fb77b
fvandepitte <frederiek.vandepitte@gmail.com>
2015-01-24 01:04:37 +0100

    Pickup

--- Project/Blackhole-Terror/Assets/Resources/Scripts/Pickup.cs
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/Pickup.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 
+    public float PickupDistance = 20f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,23 @@ public class Pickup : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetButtonDown("Pickup"))
+        {
+            Debug.Log("Pickup is hit");
+
+            float feetY = renderer.bounds.max.y;
+            var colliders = Physics2D.OverlapAreaAll(new Vector2(transform.position.x - PickupDistance, feetY - PickupDistance), new Vector2(transform.position.x - PickupDistance, feetY + PickupDistance));
+            Debug.Log(string.Format("Found {0} ", colliders.Length));
+            
+            foreach (var collider in colliders)
+            {
+                var pickupItem = collider.gameObject.GetComponent<PickupAble>();
+                if (pickupItem)
+                {
+                    DestroyImmediate(collider.gameObject);
+                }
+            }
+
+        }
 	}
 }
