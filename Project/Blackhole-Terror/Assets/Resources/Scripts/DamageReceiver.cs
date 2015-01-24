ffc39d5b1ffd2d1d22c6add7bdb94796387043cb
Disati <info@disati.be>
2015-01-24 02:33:15 +0100

    Playable thingy with assets

--- Project/Blackhole-Terror/Assets/Resources/Scripts/DamageReceiver.cs
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/DamageReceiver.cs
@@ -7,18 +7,23 @@ public class DamageReceiver : MonoBehaviour {
 	public float destroyDelay = 0.5F;
 	public GameObject explosionPrefab;
 
-	void OnTriggerEnter2D(Collider2D other) {
+	void OnTriggerStay2D(Collider2D other) {
 		if (other.tag == "Damage")
 		{
 			if (hp < 2)
 			{
-				Instantiate(explosionPrefab, new Vector3(transform.position.x, transform.position.y, -0.4F), transform.rotation);
-				Destroy (gameObject, destroyDelay);
+				rigidbody2D.AddForce(Vector2.up * 10);
+				Die ();
 			}
 			else
 			{
 			hp = hp - 1;
 			}
 		}
+	}
+
+	void Die() {
+		Instantiate(explosionPrefab, new Vector3(transform.position.x, transform.position.y, -0.4F), transform.rotation);
+		Destroy (gameObject, destroyDelay);
 	}
 }
