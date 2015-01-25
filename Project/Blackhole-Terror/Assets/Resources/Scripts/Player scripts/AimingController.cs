949cc0b3ff50b5a4ac9166801d9398b1bafdd59e
fvandepitte <frederiek.vandepitte@gmail.com>
2015-01-25 09:46:37 +0100

    Added stuff

--- Project/Blackhole-Terror/Assets/Resources/Scripts/Player scripts/AimingController.cs
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/Player scripts/AimingController.cs
@@ -5,40 +5,32 @@ public class AimingController : MonoBehaviour {
 
 	public Vector3 aimPoint;
 	public Transform aimingArrow;
-	public float aimingAngle;
+	public float aimingAngle = 0f;
 	public float rotationSpeed = 10f;
+
+    private bool gamepadMode = false;
 
 	// Use this for initialization
 	void Start () {
-	
+        gamepadMode = Input.GetJoystickNames().Length > 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!gamepadMode)
+        {
+            aimingArrow.position = this.transform.position;
+            aimPoint = Input.mousePosition;
+            aimPoint = Camera.main.ScreenToWorldPoint(aimPoint);
+            aimPoint = aimPoint - this.transform.position;
 
-		aimingArrow.position = this.transform.position;
-		aimPoint = Input.mousePosition;
-		aimPoint = Camera.main.ScreenToWorldPoint (aimPoint);
-		aimPoint = aimPoint - this.transform.position;
-
-		aimingAngle = (Mathf.Atan2(aimPoint.y, aimPoint.x) * (180 / Mathf.PI));
-
-		//aimPoint.y = aimPoint.y * 25;
-//
-//		if (aimPoint.x < 0) 
-//		{
-//			aimPoint.y += 180;	
-//		}
-//		else if (aimPoint.y < 0)
-//		{
-//			aimPoint.y += 360;
-//		}
-
-		if (aimingAngle < 0) 
-		{
-			aimingAngle += 360;
-		}
-
+            aimingAngle = (Mathf.Atan2(aimPoint.y, aimPoint.x) * (180 / Mathf.PI));
+        }
+        else
+        {
+            aimingAngle += Input.GetAxis("Vertical") * rotationSpeed / 5;   
+        }
+		
 		aimPoint = new Vector3 (0, 0, Mathf.LerpAngle(aimingArrow.eulerAngles.z, aimingAngle, rotationSpeed*Time.deltaTime));
 		aimingArrow.eulerAngles = aimPoint;
 	}
