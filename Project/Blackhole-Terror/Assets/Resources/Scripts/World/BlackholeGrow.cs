8a1fc1e8c3bab6416dc2bad18bc2c80cc96530e0
Roeland Lutters <Roeland.lutters@telenet.be>
2015-01-25 11:20:19 +0100

    Interactive blackhole

---
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/World/BlackholeGrow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackholeGrow : MonoBehaviour {
+
+	public float speed = 10f;
+	private Vector3 currentScale;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		currentScale = this.gameObject.transform.localScale;
+		this.gameObject.transform.localScale = Vector3.Lerp (currentScale, new Vector3(100,100), speed * Time.deltaTime);
+
+	}
+}
--- Project/Blackhole-Terror/Assets/Resources/Scripts/World/SceneBuildScript.cs
+++ Project/Blackhole-Terror/Assets/Resources/Scripts/World/SceneBuildScript.cs
@@ -5,12 +5,19 @@ public class SceneBuildScript : MonoBehaviour {
 
 	public GameObject[] levels;
 	public GameObject endFloor;
+	private int levelcount;
 
 	// Use this for initialization
 	void Start () {
-
+		levelcount = levels.Length;
+		DetectTopPosition (this.gameObject);
 
 	
+	}
+
+	void DetectTopPosition(GameObject level)
+	{
+
 	}
 	
 	// Update is called once per frame
